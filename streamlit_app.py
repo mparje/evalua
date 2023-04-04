98d38175d4a1746d2c7512c0e1f78289a666cbfd
mparje <126131806+mparje@users.noreply.github.com>
2023-04-04 01:22:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
+
 import streamlit as st
 import openai
 import re
-import os
+
 
 # Pide la clave de API de OpenAI al usuario
 openai.api_key = st.text_input("Introduce tu clave de API de OpenAI:")
 
+# Define function to grade essay
 def grade_essay(essay, weights):
     prompt = """
     Califica el siguiente ensayo sobre el tema "{}" en una escala del 1 al 10 según los siguientes criterios:
@@ -66,3 +68,42 @@ def grade_essay(essay, weights):
     grade = round(grade, 2)
 
     return grade
+
+# Define Streamlit app
+def app():
+    st.title("Calificador de ensayos")
+
+    st.write("Ingrese el tema del ensayo:")
+    st.session_state.topic = st.text_input("Tema", "")
+
+    st.write("Ingrese el ensayo:")
+    essay = st.text_area("Ensayo", "", height=300)
+
+    st.write("Elija los criterios de calificación y sus ponderaciones:")
+    weights = []
+    for criterion in [
+        "Contenido",
+        "Comprensión",
+        "Precisión",
+        "Creatividad",
+        "Organización",
+        "Presentación",
+        "Coherencia",
+        "Habilidad técnica",
+        "Investigación",
+        "Participación"
+    ]:
+        weight = st.slider(criterion, 0, 100, 10, 10)
+        weights.append(weight)
+
+    if sum(weights) != 100:
+        st.error("La suma de las ponderaciones debe ser 100.")
+        return
+
+    if len(essay.split()) < 500:
+        st.error("El ensayo debe tener al menos 500 palabras.")
+        return
+
+    grade = grade_essay(essay, weights)
+
+    st.write("La calificación del ensayo es: {}".format(grade))
